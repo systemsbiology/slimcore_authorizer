c3bc68fbee3640baf28e1e6a5d6fad852e5069d5
Bruz Marzolf <bmarzolf@systemsbiology.org>
2009-04-16 15:08:55 -0700

    Fix spec helper so that customer user correctly mock authorization checks.

--- lib/authenticated_spec_helper.rb
+++ lib/authenticated_spec_helper.rb
@@ -3,8 +3,8 @@ module AuthenticatedSpecHelper
     @current_user = mock_model(User, :login => "jsmith")
     User.should_receive(:find_by_login).with(@current_user.login).
       any_number_of_times.and_return(@current_user)
-    User.should_receive(:staff_or_admin?).any_number_of_times.and_return(false)
-    User.should_receive(:admin?).any_number_of_times.and_return(false)
+    @current_user.should_receive(:staff_or_admin?).any_number_of_times.and_return(false)
+    @current_user.should_receive(:admin?).any_number_of_times.and_return(false)
     request.session[:cas_user] = @current_user.login
     request.session[:user_id] = @current_user.id
   end
