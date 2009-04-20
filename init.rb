0a8f4148a23602864a43c81487d47c2713f8e414
Bruz Marzolf <bmarzolf@systemsbiology.org>
2009-04-20 09:10:25 -0700

    Reload plugin models and controllers on every request during development.

--- init.rb
+++ init.rb
@@ -1,3 +1,7 @@
+# Reload models and controllers on every request in development, to prevent association issues
+ActiveSupport::Dependencies.load_once_paths.delete(File.expand_path(File.dirname(__FILE__))+'/app/models')
+ActiveSupport::Dependencies.load_once_paths.delete(File.expand_path(File.dirname(__FILE__))+'/app/controllers')
+
 # Provide a convenient way for parent app to determine which authorizer 
 # engine is being used
 AUTHORIZER = 'slimcore'
